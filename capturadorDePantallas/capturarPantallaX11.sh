aa710a1af0dd304f97a2d95ff68ca5ba672df6a7
marcosgatica2003 <marcosgatica1112@gmail.com>
2025-06-20 22:54:05 -0300

    Cambiado de /home/marcosgatica a /tmp

--- capturadorDePantallas/capturarPantallaX11.sh
+++ capturadorDePantallas/capturarPantallaX11.sh
@@ -1,9 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash
 
-directorio=$HOME/Capturas
+directorio=/tmp
 nombreArchivo="captura_$(date +'%Y-%m-%d_%H-%M-%S').png"
 nombreRutaGuardado="$directorio/$nombreArchivo"
 export nombreRutaGuardado
 
 maim -s --quality 10 | tee >(xclip -selection clipboard -t image/png) > "$nombreRutaGuardado"
-$HOME/.config/utilidadesYSistema/capturadorDePantallas/dist/lolonCaptura
