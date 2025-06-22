6564894a46cf069759e211c7f484312b0e803293
marcosgatica2003 <marcosgatica1112@gmail.com>
2025-06-21 21:49:24 -0300

    gaticonKernel (master -> kernel-devel)

--- scriptsBash/encenderBluetooth.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Encendiendo el servicio de Bluetooth..."
-sudo systemctl unmask bluetooth.service
-sudo systemctl start bluetooth.service
-echo "Servicio de Bluetooth encendido."
-
