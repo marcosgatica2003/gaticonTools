df6fbeeb37f803d8cedc111949ccc2d6031d21a0
marcosgatica2003 <marcosgatica1112@gmail.com>
2025-07-19 19:24:50 -0300

    Agregado de scripts (circulos con colorcitos)

--- gaticonDwmBlocks/blocks.h
+++ gaticonDwmBlocks/blocks.h
@@ -1,11 +1,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/redCircle",   0, 0},
-    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/yellowCircle",   0, 0},
-    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/greenCircle",   0, 0},
+    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/redCircle",   -1, 0},
+    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/yellowCircle",   -1, 0},
+    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/greenCircle",   -1, 0},
     {"",    "/home/marcosgatica/.config/dwmblocks/scripts/volume",  2,  1},
-    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/internet.py", 5,  2},
-    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/battery", 30, 3},
+    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/internet.py", 30,  2},
+    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/battery", 5, 3},
     {"",    "/home/marcosgatica/.config/dwmblocks/scripts/hora", 5,   0},
 };
 
