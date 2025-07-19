85233b00bf44dfc8eb19db4c8fa31d2dcb1ce48b
marcosgatica2003 <marcosgatica1112@gmail.com>
2025-07-19 15:16:41 -0300

    Agregado de circulos en dwmblocks

--- gaticonDwmBlocks/blocks.h
+++ gaticonDwmBlocks/blocks.h
@@ -1,5 +1,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/redCircle",   0, 0},
+    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/yellowCircle",   0, 0},
+    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/greenCircle",   0, 0},
     {"",    "/home/marcosgatica/.config/dwmblocks/scripts/volume",  2,  1},
     {"",    "/home/marcosgatica/.config/dwmblocks/scripts/internet.py", 5,  2},
     {"",    "/home/marcosgatica/.config/dwmblocks/scripts/battery", 30, 3},
