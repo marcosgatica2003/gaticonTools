fd8a618aa3cf3f4318bc86300625b619049582dd
marcosgatica2003 <marcosgatica1112@gmail.com>
2025-07-18 21:56:40 -0300

    Agregado de parche gruvbox dark

--- gaticonSt/config.h
+++ gaticonSt/config.h
@@ -92,55 +92,26 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
-
-/* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	// /* 8 normal colors */
-	// "black",
-	// "red3",
-	// "green3",
-	// "yellow3",
-	// "blue2",
-	// "magenta3",
-	// "cyan3",
-	// "gray90",
-
-	// // /* 8 bright colors */
-	// "gray50",
-	// "red",
-	// "green",
-	// "yellow",
-	// "#5c5cff",
-	// "magenta",
-	// "cyan",
-	// "white",
-
-	"#282828", // black
-	"#cc241d", // red
-	"#98971a", // green
-	"#ffe100", // yellow
-	"#458588", // blue
-	"#b16286", // magenta
-	"#689d6a", // cyan
-	"#a89984", // white (light gray)
+	/* 8 normal colors */
+	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1] = "#cc241d", /* red     */
+	[2] = "#98971a", /* green   */
+	[3] = "#d79921", /* yellow  */
+	[4] = "#458588", /* blue    */
+	[5] = "#b16286", /* magenta */
+	[6] = "#689d6a", /* cyan    */
+	[7] = "#a89984", /* white   */
 
 	/* 8 bright colors */
-	"#928374", // bright black (gray)
-	"#fb4934", // bright red
-	"#b8bb26", // bright green
-	"#fabd2f", // bright yellow
-	"#83a598", // bright blue
-	"#d3869b", // bright magenta
-	"#8ec07c", // bright cyan
-	"#ebdbb2", // bright white
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"#ffffff", /* default foreground colour */
-	"#222222", /* default background colour */
+	[8]  = "#928374", /* black   */
+	[9]  = "#fb4934", /* red     */
+	[10] = "#b8bb26", /* green   */
+	[11] = "#fabd2f", /* yellow  */
+	[12] = "#83a598", /* blue    */
+	[13] = "#d3869b", /* magenta */
+	[14] = "#8ec07c", /* cyan    */
+	[15] = "#ebdbb2", /* white   */
 };
 
 
@@ -148,9 +119,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
