253e893082485f4bb47efed02a8cfc1943f02f33
marcosgatica2003 <marcosgatica1112@gmail.com>
2025-07-31 21:10:03 -0300

    Apuntando a binario internet & borrado de circulos

--- gaticonDwmBlocks/blocks.h
+++ gaticonDwmBlocks/blocks.h
@@ -1,10 +1,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/redCircle",   -1, 0},
-    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/yellowCircle",   -1, 0},
-    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/greenCircle",   -1, 0},
     {"",    "/home/marcosgatica/.config/dwmblocks/scripts/volume",  2,  1},
-    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/internet.py", 30,  2},
+    {"",    "/home/marcosgatica/.config/dwmblocks/scripts/dist/internet/internet", 30,  2},
     {"",    "/home/marcosgatica/.config/dwmblocks/scripts/battery", 5, 3},
     {"",    "/home/marcosgatica/.config/dwmblocks/scripts/hora", 5,   0},
 };
